ec7a7867406080a7adb8195ad45695cc7e87db46
Pablo H. Paladino <paladinop@gmail.com>
2015-07-16 20:09:50 -0300

    data desde spreadsheet + tabletop + transparency

--- js/main.js
+++ js/main.js
@@ -13,15 +13,59 @@ var Chequeado;
 
     Chequeado.mainLayer;
 
-    Chequeado.init = function(cartoId){
+    Chequeado.ID;
 
+    Chequeado.init = function(){
+
+        var key = Chequeado.getParam('key');
+        Chequeado.ID = Chequeado.getParam('ID');
+        if(key){
+            try {
+                Tabletop.init( { key: key,
+                        callback: Chequeado.dataLoaded,
+                        parseNumbers: true,
+                        simpleSheet: true 
+                    });
+            }
+            catch(err) {
+                alert('Error al leer la planilla: '+err.message);
+            }
+        }else{
+            alert('Falta el parámetro "key" o en parámetro "ID" en la url.');
+        }
+
+    };
+
+    Chequeado.dataLoaded = function(data,tabletop) {
+        Chequeado.data = data.filter(function(e){
+            return (e.identificador == Chequeado.ID);
+        });
+        Chequeado.data = Chequeado.data[0];
+        Chequeado.startMap(Chequeado.data.mapID);
+        $('.modal').render(Chequeado.data);
+    }
+
+    Chequeado.startMap = function(cartoId){
         var cartoUrl = 'https://chequeado.cartodb.com/api/v2/viz/'+cartoId+'/viz.json';
 
-		cartodb.createVis('chequeado-map', cartoUrl)
-		  .done(function(vis, layers) {
-		    
-		});
+        cartodb.createVis('chequeado-map', cartoUrl)
+          .done(function(vis, layers) {
+            
+        });
 
+    }
+
+    Chequeado.initGenerador = function(){
+        $('#test').on('click',function(){
+            var sheet = $('#sheet').val();
+            var id = $('#id').val();
+
+            if(sheet && id){
+                var url = location.origin + location.pathname + 'viz.html?key='+sheet+'&ID='+id;
+                $('iframe').attr('src',url);
+                $('textarea').html('<iframe src="'+url+'" frameborder="0" height="500" width="100%"></iframe>');
+            }
+        });
     };
 
     Chequeado.getParam = function(name) {
